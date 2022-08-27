d56c82d6756f4fbc1edf30457663344e70502378
CosmicInsight <sissirabii15@gmail.com>
2022-08-27 17:20:48 +0100

    init

---
+++ src/main/java/com/sissi/dev/deploy1o1/Deploy1o1Application.java
@@ -0,0 +1,13 @@
+package com.sissi.dev.deploy1o1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy1o1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy1o1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sissi/dev/deploy1o1/Deploy1o1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sissi.dev.deploy1o1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy1o1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
